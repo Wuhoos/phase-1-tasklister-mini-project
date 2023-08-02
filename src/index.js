5382bf835213288dac61474ba14263b210f70ab1
David <wjw3298@gmail.com>
2023-08-02 19:12:16 -0400

    complete version 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById('create-task-form')
+
+  newTaskForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const newTaskInput = document.getElementById('new-task-description').value
+    const newTask = document.createElement('li')
+
+    newTask.textContent = newTaskInput;
+
+   // console.log(newTask)
+
+    addTasksToList(newTask)
+    //newTask.appendChild(button)
+
+    const button = document.createElement('button')
+    // <button></button>
+    button.textContent = "X"
+    // <button>X</button>
+    newTask.appendChild(button)
+    
+
+
+
+    button.addEventListener('click', () => {
+      newTask.remove()
+    })
+
+  })
+
+  // create a button
+
+
+  function addTasksToList(task){
+    const toDoList = document.getElementById("list")
+    // const newTask = document.createElement('li')
+
+    //console.log(newTask)
+    toDoList.appendChild(task)
+
+    newTaskForm.reset()
+  }
+
+  // const button = document.createElement('Button')
+  //button.textContent ="X"
+  //newTask.appendChild('button')
+
+
 });
+
